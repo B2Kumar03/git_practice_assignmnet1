c2f946a69ecced34954b96f2af05c2a004258708
Bittu Kumar <bk7355583@example.com>
2023-10-12 17:37:17 +0530

    A file is Added

---
+++ index.js
@@ -0,0 +1,15 @@
+function prime(n){
+    var flag=true;
+    for(var i=1;i<n;i++){
+        if(n%i==0){
+            flag=false;
+
+        }
+        if(falg==true){
+            console.log("Number is Prime...");
+        }
+        else{
+            console.log("Number is Prime...");
+        }
+    }
+}
